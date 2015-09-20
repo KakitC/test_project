ae8d99b8c2cbbbdde23eea48b2a921aef07439e4
Kakit Cheung <kitkatcheung@gmail.com>
2015-09-19 23:02:14 -0700

    Added Windows vs Linux operability

--- countMicrosTimer.py
+++ countMicrosTimer.py
@@ -3,16 +3,21 @@
 
 __author__ = 'kakit'
 
-import time
-
-#Count how many times I can call something in 1ms
-#Raw time.clock() call speed: 3500/ms
+import platform
+OS = platform.system()
+if OS == "Windows":
+    from time import clock as time
+elif OS == "Linux":
+    from time import clock as time
+    
+# Count how many times I can call something in 1ms
+# Raw time() call speed: 3500/ms
 # With other stuff: 2500/ms
 # With 1 print statement: 200/ms
-deltaTime = time.clock()
+deltaTime = time()
 count = 0
 while deltaTime < .001:
-    deltaTime = time.clock()
+    deltaTime = time()
     count += 1
     print deltaTime
     print count
@@ -20,13 +25,13 @@ print "Iterations in 1ms with print statement: ", count
 
 # Track processor clock precision,
 # what's the error on each call at 1us period, accumulated error
-startTime = time.clock()
+startTime = time()
 deltaTime = 0
 iters = 800
 count = 0
 saveTime = []
 while deltaTime < .001: # less than 1ms
-    deltaTime = time.clock() - startTime
+    deltaTime = time() - startTime
     if deltaTime > ((count + 1) / 1000.0 / iters):
         count +=1
         saveTime.append(deltaTime)
